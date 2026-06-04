5ce41675c67a6142def1c02e38f0d7dc40f08951
Fani-Attock <fanikhan3293@gmail.com>
2026-06-04 11:29:21 +0500

    fix vercel serverless backend

--- backend/server.mjs
+++ backend/server.mjs
@@ -1,13 +1,16 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
-// routes
+// test route
 app.get("/", (req, res) => {
   res.json({ message: "Backend running 🚀" });
 });
 
-// IMPORTANT for Vercel
+// IMPORTANT: NO app.listen()
+
 export default app;
